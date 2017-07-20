f1e88e77d741cb8baa6dff8b66a432a6b1e69f18
zjjfly <zjjblue@gmail.com>
2017-07-20 23:36:56 +0800

    Initial commit

---
+++ src/test/java/chp01/Traps1.java
@@ -0,0 +1,84 @@
+package chp01;
+
+import java.math.BigDecimal;
+
+public class Traps1 {
+	//@Test
+	// 陷阱1 奇数判断
+	public void isOdd() {
+		int i = 3;// 可以赋任意值
+		System.out.println((i & 1) != 0);
+	}
+
+	//@Test
+	// 陷阱2 浮点计算
+	public void Change() {
+		// 这种方式有误差
+		// System.out.println(2.0-1.1);
+		// System.out.printf("%.2f%n",2.0-1.1);
+		// 无误差的方式
+		System.out.println(new BigDecimal("2.0")
+				.subtract(new BigDecimal("1.1")));
+		// 错误的bigdecimal用法
+		// System.out.println(new BigDecimal(2.0-1.1));
+	}
+
+	//@Test
+	// 陷阱3 长整除
+	public void LongDivision() {
+		// 错误的赋值方式
+		// final long DAY_MICROS=24*60*60*1000*1000;
+		// final long DAY_MILLIS=24*60*60*1000;
+		final long DAY_MICROS = 24l * 60 * 60 * 1000 * 1000;
+		final long DAY_MILLIS = 24l * 60 * 60 * 1000;
+		System.out.println(DAY_MICROS / DAY_MILLIS);
+	}
+
+	//@Test
+	// 陷阱4 注意数字1和小写字母l，它们真他妈的像！
+	public void Iand1() {
+		System.out.println(12345 + 5432l);
+	}
+
+	//@Test
+	// 陷阱5 混合类型计算问题，最好避免混合类型计算！
+	public void JoyofHex() {
+		System.out.println(Long.toHexString(0x100000000L + 0xcafebabe));
+	}
+
+	//@Test
+	// 陷阱6 多重转型
+	// byte（八位）有符号，是用二进制补码的形式表示，11111111,转成无符号的char（16位），变为1111111111111111，转成十进制就是65535
+	public void MultiCast() {
+		System.out.println((int) (char) (byte) -1);
+	}
+
+	//@Test
+	// 陷阱7 在单个表达式中不要对一个变量赋值两次！
+	public void CleverSwap() {
+		int x = 3;
+		int y = 2;
+		x ^= y ^= x ^= y;
+		System.out.println("x=" + x + ",y=" + y);
+
+	}
+
+	//@Test
+	// 陷阱8 最好在条件表达式中使用相同类型的第二个和第三个操作数
+	public void DoSequis() {
+		char x = 'X';
+		int i = 0;
+		System.out.println(true ? x : 0);
+		System.out.println(false ? i : x);
+	}
+	
+	//@Test
+	// 陷阱9 不要将复合赋值表达式作用于byte，short，char类型的变量，当结果类型宽于变量的类型是，会产生一个窄化转型
+	public void Half() {
+		short x = 1;
+		int i = 123546;
+		x += i;
+		System.out.println(x);
+	}
+
+}
---
+++ src/test/java/chp02/Traps2.java
@@ -0,0 +1,88 @@
+package chp02;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Pattern;
+
+
+
+public class Traps2 {
+	//@Test
+	// 陷阱1 字符相加输出
+	public void LastLaugh() {
+		System.out.println('h' + 'a');
+		System.out.println("h"+"a");
+		System.out.println("2+2="+2+2);
+	}
+
+	//@Test
+	// 陷阱2 char数组没有实现toString方法
+	public void ABC() {
+		String s = "This is ";
+		char[] cs = new char[] { 'a', 'b', 'c' };
+		System.out.println(s + cs);
+		System.out.println(s + String.valueOf(cs));
+	}
+
+	//@Test
+	// 陷阱3 +好无论做运算符还是字符串连接符都比==的优先级高
+	public void AnimalFarm() {
+		String s = "my dog7";
+		String p = "my dog" + s.length();
+		System.out.println("This is " + s == p);
+	}
+	//@Test
+	//陷阱4 unicode转义字符会在编译时转换成它所表示的字符
+	public void EscapeRout(){
+		System.out.println("a\u0022.length()+\u0022b".length());
+		//\u0022其实是一个双引号。
+	}
+
+	//@Test
+	//陷阱5 还是unicode转义符产生的问题
+	public void LinePrinter(){
+		//下面的这行注释如果没有第二个"//"会出问题
+		// \u000A //是换行符的unicode转义符
+		char c=0x000A;
+		System.out.println(c);
+		System.out.println("line");
+	}
+	//@Test
+	//陷阱6 把byte数组转成string时，都在使用一个字符集，不管是否显示的指定
+	public void StringCheese() throws UnsupportedEncodingException {
+		byte bytes[]=new byte[256];
+		for(int i=0;i<256;i++){
+			bytes[i]=(byte)i;
+		}
+		//不指定字符集会出问题
+		//String str=new String(bytes,"ISO-8859-1");
+		String str=new String(bytes);
+		for(int i=0,n=str.length();i<n;i++){
+			System.out.println((int)str.charAt(i)+" ");
+
+		}
+	}
+//	@Test
+	//陷阱7 注释掉一段代码最好不要用块注释，而是用单行注释
+//	public void sum() {
+//		/*
+//		/* Add  the number from 1 to n*/
+//		int sum=0;
+//		for(int i=0;i<100;i++){
+//			sum+=i;
+//		}
+//		System.out.println(sum);
+//		*/
+//	}
+	@Test
+	//陷阱8 正则表达式中一些符号要加转义字符
+	public void WhatisMyClass(){
+		//没有加转义字符
+		System.out.println(Traps2.class.getName().replaceAll(".", "/"));
+		//加了转移字符
+		System.out.println(Traps2.class.getName().replaceAll("\\.", "/"));
+		//jdk5提供的新方法
+		System.out.println(Traps2.class.getName().replaceAll(Pattern.quote("."), "/"));
+	}
+}
